cdfeecaac1d1853b095808d4ec2a875a1658fbe3
wd <168116060@qq.com>
2021-08-10 19:41:14 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,9 +4,12 @@ package com.atguigu.git;
  * @ClassName GitTest
  * @Description TODO
  * @Author 16811_WD
- * @Date 2021/8/10 18:42
+ * @Date 2021/8/10 19:39
  * @Version 1.0
  */
 
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
